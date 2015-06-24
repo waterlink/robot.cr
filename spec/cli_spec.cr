4f9c1ff14cfcc047a311cd11fbef57ba56993b41
Oleksii Fedorov <waterlink000@gmail.com>
2015-06-25 00:14:49 +0300

    Cli stub implementation [green]

--- spec/cli_spec.cr
+++ spec/cli_spec.cr
@@ -12,23 +12,23 @@ module Robot
         "report",
       ]
 
-      stdin = StringIO.new(commands.join("\n"))
+      stdin = StringIO.new(commands.join("\n") + "\n")
       stdout = StringIO.new(capacity: 4096)
       cli = Cli.new(stdin: stdin, stdout: stdout)
 
       cli.start
 
-      stdout.read.split("\n").should eq([
+      stdout.read.should eq([
         "robot :001 > ",
         "robot :002 > ",
         "robot :003 > ",
         "robot :004 > ",
         "robot :005 > ",
         "robot :006 > ",
-        "=> 2,1,north",
+        "=> 2,1,north\n",
         "robot :007 > ",
-        "exit",
-      ])
+        "exit\n",
+      ].join)
     end
   end
 end
--- src/cli.cr
+++ src/cli.cr
@@ -1,9 +1,31 @@
 module Robot
   class Cli
-    def initialize(stdin = STDIN, stdout = STDOUT)
+    getter stdin
+    getter stdout
+    getter counter
+
+    def initialize(@stdin = STDIN, @stdout = STDOUT)
+      @counter = 0
     end
 
     def start
+      prompt
+      stdin.each_line do |line|
+        if line.strip == "report"
+          stdout.puts("=> 2,1,north")
+        end
+        prompt
+      end
+      stdout.puts("exit")
+    end
+
+    private def increase_counter
+      @counter += 1
+    end
+
+    private def prompt
+      increase_counter
+      stdout.printf("robot :%03d > ", counter)
     end
   end
 end
