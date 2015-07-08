9ac17aaff5900de8c491957b3c934d054a02e3bd
Oleksii Fedorov <waterlink000@gmail.com>
2015-07-09 01:21:00 +0300

    Extract general view concept as a macro [refactor]

---
+++ spec/view_spec.cr
@@ -0,0 +1,43 @@
+require "./spec_helper"
+require "../src/view"
+
+module Robot
+  view TestView, [hello, world] do
+    io << "hello: #{hello}, world: #{world}"
+  end
+
+  view EmptyTestView do
+    io << ""
+  end
+
+  describe TestView do
+    it "works correctly" do
+      TestView.new("hi", "earth").to_s
+        .should eq("hello: hi, world: earth")
+    end
+
+    it "handles equality correctly" do
+      TestView.new("hi", "earth")
+        .should eq(TestView.new("hi", "earth"))
+
+      TestView.new("hi", "earth")
+        .should_not eq(TestView.new("hello", "earth"))
+      TestView.new("hi", "earth")
+        .should_not eq(TestView.new("hi", "world"))
+
+      TestView.new("hi", "earth")
+        .should_not eq(EmptyTestView.new)
+    end
+  end
+
+  describe EmptyTestView do
+    it "works correctly" do
+      EmptyTestView.new.to_s.should eq("")
+    end
+
+    it "handles equality correctly" do
+      EmptyTestView.new.should eq(EmptyTestView.new)
+      EmptyTestView.new.should_not eq(["hello world"])
+    end
+  end
+end
---
+++ src/view.cr
@@ -0,0 +1,30 @@
+module Robot
+  macro view(name, params=[] of ASTNode, &block)
+    struct {{name.id}}
+      {{
+        params.map do |param|
+          "protected getter #{param.id}".id
+        end.join("\n").id
+      }}
+
+      def initialize({{ params.map { |p| "@#{p.id}".id }.argify }})
+      end
+
+      def to_s(io)
+        {{ block.body }}
+      end
+
+      def ==(other : {{ name.id }})
+        ({{
+          params.map do |param|
+            "self.#{param.id} == other.#{param.id}".id
+          end
+        }} of Bool).all?
+      end
+
+      def ==(other)
+        false
+      end
+    end
+  end
+end
