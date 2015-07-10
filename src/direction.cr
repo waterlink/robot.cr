78b66d25fc985669c4737d4bc0916b36ed2a71e5
Oleksii Fedorov <waterlink000@gmail.com>
2015-07-11 01:53:13 +0300

    Fix broken spec in InputController [green + red]

--- src/direction.cr
+++ src/direction.cr
@@ -1,11 +1,18 @@
 module Robot
   class Direction
-    def self.north; new(0, -1) end
-    def self.east; new(1, 0) end
-    def self.south; new(0, 1) end
-    def self.west; new(-1, 0) end
+    def self.north; new(0, -1, "north") end
+    def self.east; new(1, 0, "east") end
+    def self.south; new(0, 1, "south") end
+    def self.west; new(-1, 0, "west") end
 
-    def initialize(@left, @top)
+    protected getter left, top, name
+
+    def initialize(@left, @top, @name)
+    end
+
+    def ==(other : Direction)
+      self.left == other.left &&
+        self.top == other.top
     end
   end
 end
--- src/position.cr
+++ src/position.cr
@@ -1,6 +1,13 @@
 module Robot
   class Position
+    protected getter left, top
+
     def initialize(@left, @top)
+    end
+
+    def ==(other : Position)
+      self.left == other.left &&
+        self.top == other.top
     end
   end
 end
