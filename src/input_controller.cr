93d7c0855ba7550358ef6c9ae182c1fa5239f551
Oleksii Fedorov <waterlink000@gmail.com>
2015-07-13 04:13:14 +0300

    InputController: Reduce everything to usage of state [refactor]

--- src/input_controller.cr
+++ src/input_controller.cr
@@ -2,59 +2,54 @@ require "./view"
 
 module Robot
   class InputController
-    private getter board, robot, state
+    private getter state
 
     def initialize
-      @board = Board.new(10, 10)
-      @robot = Robot.new
+      board = Board.new(10, 10)
+      robot = Robot.new
       @state = State.new(board, robot)
     end
 
     def handle(line)
+      @state = new_state(line)
+      state.view
+    end
+
+    def new_state(line)
       handle_report(line) ||
         handle_place(line) ||
         handle_forward(line) ||
         handle_left(line) ||
         handle_right(line) ||
-        EmptyView.new
+        state.augment
     end
 
     def handle_report(line)
       return unless line == "report"
       @state = ReportCommand.new(state).call
-      state.view
     end
 
     def handle_place(line)
       command, args = line.split(" ")
       return unless command == "place"
-
-      @state = PlaceCommand.new(state, args.split(",")).call
-      @robot = state.robot
-      nil
+      PlaceCommand.new(state, args.split(",")).call
     rescue IndexOutOfBounds
       nil
     end
 
     def handle_forward(line)
       return unless line == "forward"
-      @robot = robot.forward
-      @state = state.augment(robot: robot)
-      nil
+      state.augment(robot: state.robot.forward)
     end
 
     def handle_left(line)
       return unless line == "left"
-      @robot = robot.left
-      @state = state.augment(robot: robot)
-      nil
+      state.augment(robot: state.robot.left)
     end
 
     def handle_right(line)
       return unless line == "right"
-      @robot = robot.right
-      @state = state.augment(robot: robot)
-      nil
+      state.augment(robot: state.robot.right)
     end
   end
 
